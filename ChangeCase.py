5bf03a6136e2cc5ee795e34cd64fd0fed6b6a7e3
vaibhavchavan01 <vaibhavchavaan495@gmaail.com>
2021-12-05 22:45:44 +0530

    String1 branch 5 code are commited

--- ChangeCase.py
+++
@@ -1,7 +0,0 @@
-'''Write the function changeCase(word) that changes 
-the case of all the letters in a word and returns the new word.'''
-
-def changeCase(word):
-	return word.swapcase()
-
-print(changeCase("Apple"))
